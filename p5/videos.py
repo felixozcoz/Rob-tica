d498be8f51dd7b333b278593deba4f496c3a4b8e
Dede <111243030+dedetheprogrammer@users.noreply.github.com>
2024-05-17 07:57:51 +0200

    mas

--- p5/videos.py
+++ p5/videos.py
@@ -1,6 +1,7 @@
 from Robot import Robot
 from ReMapLib import Map
-from geometry import Vector2
+# from geometry import Vector2
+
 
 print("Inicializando el robot y sus componentes")
 robot = Robot()
@@ -12,3 +13,6 @@ robot.playTrajectory(points, 30, showPlot=True)
 
 rmap = Map("mapa_video0.txt", [0,1], [2,1])
 rmap = Map("mapa_video1.txt", [0,0], [2,2])
+robot.playMap(v_0=20, w_0=1)
+
+robot.trackObject((80, 70, 50), (100, 255, 255))
